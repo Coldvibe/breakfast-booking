81b7c285332bf84e668075b975cc21ea3a62fff4
Coldvibe <benoit.delmelle@gmail.com>
2026-03-19 14:52:20 +0000

    debug login

--- app/static/service-worker.js
+++ app/static/service-worker.js
@@ -25,8 +25,16 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
